278decb384f2ce681d54dcfe6e906f75b7c17c88
Tococo852 <tococo852@gmail.com>
2025-04-08 14:42:05 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+a mockup library using javascript, no memory storage planned for it, books banish after closing the browser
---
+++ index.js
@@ -0,0 +1,13 @@
+function Book(title, author,pages,readed){
+    if(!new.target){
+        throw Error("you must use 'new' operator to call this constructor")
+    }
+    this.title=title;
+    this.author=author;
+    this.pages=pages;
+    this.readed=readed;
+}
+Book.prototype.info= function(){
+    console.log(`${this.title} by ${this.author}, ${this.pages} pages, ${this.readed? 'Readed':'Not Readed'}`)
+
+}
