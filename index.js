60d2bbc40ac90cba2bc018154efc60e648c9c92b
Tococo852 <tococo852@gmail.com>
2025-04-09 16:38:05 -0400

    made a form and a js function to read it

--- index.js
+++ index.js
@@ -20,3 +20,21 @@ function addBookToLibrary(title,author,pages,readed){
 
 }
 
+function getFormData(){
+    /* this function does a querryselectAll on the form to grab all the inputs, then it turns the querry into an array to work with array functions
+    after that using the filter fuction, the radio option that was not selected is removed from the array, after which the array is reduced to an object
+    that containt all the book information, is basically creating the book object
+    
+    the object returned has the following format
+    
+    bookName: "string" //book name
+    author: "string" //author name
+    pages: number // an int with the amount of pages the book has
+    read: 'string'// a string that can only be 'false' or 'true', for the sake of it being worked as a boolean later
+    */
+    bookLike=Array.from(document.querySelectorAll('#bookForm input')).filter((input)=>!(input.type=='radio' && !input.checked)).reduce((acc,input)=>(
+        {...acc, [input.id]: input.value}),{});
+    return new Book(bookLike.bookName,bookLike.author, bookLike.pages, bookLike.read=="true");
+
+}
+
